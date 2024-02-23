b21b8255121b601876bfb60639571538f32b3ec2
yiwenzcmu <yiwenemmaz@gmail.com>
2024-02-23 01:24:13 -0500

    https://github.com/yiwenzcmu/MLIP_Lab6.git

--- test_utility.py
+++ test_utility.py
@@ -37,5 +37,7 @@ def feature_target_sample(housing_data_sample):
 
 def test_data_split(feature_target_sample):
     return_tuple = data_split(*feature_target_sample)
-    # TODO test if the length of return_tuple is 4
-    raise NotImplemented
+    # test if the length of return_tuple is 4
+    assert len(return_tuple) == 4
+
+    # raise NotImplemented
